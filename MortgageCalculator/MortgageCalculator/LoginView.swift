9f1c31f21d37afff5467f5d69716ceb7802c0d9d
Thuwaybah Sheikh <thuwaybah09@gmail.com>
2024-08-14 20:36:21 -0700

    Splash screen + login page

---
+++ MortgageCalculator/MortgageCalculator/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  MortgageCalculator
+//
+//  Created by Thuwaybah Sheikh on 8/12/24.
+//
+
+import Foundation
---
+++ MortgageCalculator/MortgageCalculator/SplashScreenView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashScreenView.swift
+//  MortgageCalculator
+//
+//  Created by Thuwaybah Sheikh on 8/14/24.
+//
+
+import Foundation
